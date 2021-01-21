866b5c993a2cef7c15832c35315be4d208bfd837
Tej Chajed <tchajed@mit.edu>
2021-01-20 19:22:04 -0600

    Pass over lang.v
    Along the way finished proving expr is countable

--- src/lang.v
+++ src/lang.v
@@ -3,8 +3,23 @@ From stdpp Require Import gmap.
 From iris.algebra Require Export ofe.
 From iris.program_logic Require Export language ectx_language ectxi_language.
 From iris.prelude Require Import options.
-
 Open Scope Z.
+
+(*|
+Syntax and semantics of simp_lang.
+
+We define a lambda calculus with heap operations and Fork, much as in heap_lang. Binders are represented as strings and substitution is *not* capture-avoiding; instead, we only reason about applications to closed terms, in which case this substitution function is well-behaved. To make this possible, all values are identified by a dedicated `Val` constructor in `expr`. One result is that there is a distinction between Rec (a potentially recursive lambda **`expr`** ) and RecV (a lambda **`val`** ), and similarly for the Pair expression and PairV value. The expression immediately reduces to the value.
+
+Values are extremely simple in this language - they are either integers or booleans. Locations are represented with integers and booleans as 0 and 1. This makes simp_lang not great for logical relations proofs or a decent type system (unlike heap_lang), but that's okay for pedagogical purposes here.
+
+The language semantics is simplified by instantiating the `ectxi_language` interface, for languages defined in terms of *evaluation context items*, in line with how a semantics might be written on a blackboard. We'll get to exactly what that means later, but basically we will write down "head reduction" rules for each primitive when its arguments are values and evaluation contexts for the other reductions and `ectxi_language` will do the rest. You could also directly instantiate `language` and not use evaluation contexts.
+
+===========
+Syntax
+===========
+
+We will be careful to minimize the recursive structure of `expr` to make some administrative stuff later on simpler. For example we group plus, equals, and pair into a single `expr` constructor that takes two expressions.
+|*)
 
 Inductive base_lit :=
   | LitInt (n:Z)
@@ -24,6 +39,10 @@ Inductive heap_op :=
   | LoadOp
   | StoreOp
   | GetSetOp.
+
+(*|
+Expressions are defined mutually recursively with values. As explained above, an expression is a value iff it uses the Val constructor, which makes defining reduction and substitution much simpler, but requires duplicating `Rec` and `Pair` to `val` as `RecV` and `PairV`.
+|*)
 
 Inductive expr :=
   (* Values *)
@@ -49,6 +68,10 @@ with val :=
 Bind Scope expr_scope with expr.
 Bind Scope val_scope with val.
 
+(*|
+These define part of ectxi_language: we need a type for expressions, a type for values, and a way to go back and forth (where to_val is partial because only some expressions are values). Here the use of Val to identify all values makes this easy and non-recursive.
+|*)
+
 Notation of_val := Val (only parsing).
 
 Definition to_val (e : expr) : option val :=
@@ -56,19 +79,6 @@ Definition to_val (e : expr) : option val :=
   | Val v => Some v
   | _ => None
   end.
-
-Fixpoint val_comparable (v : val) : Prop :=
-  match v with
-  | LitV _ => True
-  | RecV _ _ _ => False
-  | PairV v1 v2 => val_comparable v1 ∧ val_comparable v2
-  end.
-
-Definition loc := Z.
-
-Record state : Type := {
-  heap: gmap loc val;
-}.
 
 (** Equality and other typeclass stuff *)
 Lemma to_of_val v : to_val (of_val v) = Some v.
@@ -79,6 +89,10 @@ Proof. destruct e=>//=. by intros [= <-]. Qed.
 
 Global Instance of_val_inj : Inj (=) (=) of_val.
 Proof. intros ??. congruence. Qed.
+
+(*|
+We will now do a bunch of boring work to prove that expressions have decidable equality and are countable for technical reasons.
+|*)
 
 Global Instance base_lit_eq_dec : EqDecision base_lit.
 Proof. solve_decision. Defined.
@@ -148,20 +162,97 @@ Proof.
   destruct x; auto.
 Qed.
 
+Global Instance heap_op_countable : Countable heap_op.
+Proof.
+  refine (inj_countable'
+            (λ op, match op with | AllocOp => 0 | LoadOp => 1 | StoreOp => 2 | GetSetOp => 3  end)
+            (λ n, match n with | 0 => _ | 1 => _ | 2 => _ | 3 => _ | _ => ltac:(constructor) end) _).
+  destruct x; auto.
+Qed.
+
 Global Instance expr_countable : Countable expr.
 Proof.
-Admitted.
+ set (enc :=
+   fix go e :=
+     match e with
+     | Val v => GenNode 0 [gov v]
+     | Var x => GenLeaf (inl (inl x))
+     | Rec f x e => GenNode 1 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); go e]
+     | App e1 e2 => GenNode 2 [go e1; go e2]
+     | UnOp op e => GenNode 3 [GenLeaf (inr (inr (inl op))); go e]
+     | BinOp op e1 e2 => GenNode 4 [GenLeaf (inr (inr (inr (inl op)))); go e1; go e2]
+     | HeapOp op e1 e2 => GenNode 5 [GenLeaf (inr (inr (inr (inr op)))); go e1; go e2]
+     | If e0 e1 e2 => GenNode 6 [go e0; go e1; go e2]
+     | Fork e => GenNode 7 [go e]
+     end
+   with gov v :=
+     match v with
+     | LitV l => GenLeaf (inr (inl l))
+     | RecV f x e =>
+        GenNode 0 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); go e]
+     | PairV v1 v2 => GenNode 1 [gov v1; gov v2]
+     end
+   for go).
+ set (dec :=
+   fix go e :=
+     match e with
+     | GenNode 0 [v] => Val (gov v)
+     | GenLeaf (inl (inl x)) => Var x
+     | GenNode 1 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); e] => Rec f x (go e)
+     | GenNode 2 [e1; e2] => App (go e1) (go e2)
+     | GenNode 3 [GenLeaf (inr (inr (inl op))); e] => UnOp op (go e)
+     | GenNode 4 [GenLeaf (inr (inr (inr (inl op)))); e1; e2] => BinOp op (go e1) (go e2)
+     | GenNode 5 [GenLeaf (inr (inr (inr (inr op)))); e1; e2] => HeapOp op (go e1) (go e2)
+     | GenNode 6 [e0; e1; e2] => If (go e0) (go e1) (go e2)
+     | GenNode 7 [e] => Fork (go e)
+     | _ => Val $ LitV LitUnit (* dummy *)
+     end
+   with gov v :=
+     match v with
+     | GenLeaf (inr (inl l)) => LitV l
+     | GenNode 0 [GenLeaf (inl (inr f)); GenLeaf (inl (inr x)); e] => RecV f x (go e)
+     | GenNode 1 [v1; v2] => PairV (gov v1) (gov v2)
+     | _ => LitV LitUnit (* dummy *)
+     end
+   for go).
+ refine (inj_countable' enc dec _).
+ refine (fix go (e : expr) {struct e} := _ with gov (v : val) {struct v} := _ for go).
+ - destruct e as [v| | | | | | | |]; simpl; f_equal;
+     [exact (gov v)|done..].
+ - destruct v; by f_equal.
+Qed.
 Global Instance val_countable : Countable val.
 Proof. refine (inj_countable of_val to_val _); auto using to_of_val. Qed.
 
+(*|
+===========
+Semantics
+===========
+
+Now we can get to the fun stuff - the semantics!
+
+This is a really simple language so its state is just a finite mapping from locations (integers) to values.
+|*)
+
+Definition loc := Z.
+
+Record state : Type := {
+  heap: gmap loc val;
+}.
+
+(** the language interface needs these things to be inhabited, I believe *)
 Global Instance state_inhabited : Inhabited state :=
   populate {| heap := inhabitant; |}.
 Global Instance val_inhabited : Inhabited val := populate (LitV LitUnit).
 Global Instance expr_inhabited : Inhabited expr := populate (Val inhabitant).
 
-Canonical Structure stateO := leibnizO state.
-Canonical Structure valO := leibnizO val.
-Canonical Structure exprO := leibnizO expr.
+(*|
+Evaluation contexts define what the next redex is, meaning what the evaluation order is. We use a right-to-left evaluation order only to match heap_lang - see its documentation for why that's useful (it has to do with giving specifications for higher-order functions, nothing that'll come up here).
+
+Evaluation contexts here are commonly seen in a basic presentation on the lambda calculus, but we give a brief review anyway. You should read AppRCtx e1 the way we'd write e □ on the board - that is, we can reduce the argument to an application at any time. AppLCtx v2 is like □ v, which says that we can reduce an application's function once the right-hand side is a value. These two rules are what determine a right-to-left evaluation order, and we consistently follow that convention for binary operations and heap operations as well.
+
+Technically these evaluation contexts items are given meaning by `fill_item` below, which specifies how to combine an evaluation context with an expression that goes in the "hole". Note that we define only evaluation context **items** here, so that the holes are not recursive. To make them recursive `ectxi_language` defines complete evaluation contexts as `list ectx_item`.
+|*)
 
 (** Evaluation contexts *)
 Inductive ectx_item :=
@@ -187,6 +278,10 @@ Definition fill_item (Ki : ectx_item) (e : expr) : expr :=
   | HeapOpRCtx op e1 => HeapOp op e1 e
   end.
 
+(*|
+Context reductions define all the "boring" reduction rules; now we need to define how to reduce each constructor for `expr` when applied to values, the "head reduction" rules. The most important of these is the beta reduction rule for `App (RecV f x v1) v2`, namely substitution. Substitution is not capture avoiding but does not recurse into values (identified by the `Val` constructor, once again), which works properly as long as the expressions e are closed. This substitution definition only handles the non-recursive part of functions; the beta rule will substitute the function for itself recursively.
+|*)
+
 (** Substitution *)
 Fixpoint subst (x : string) (v : val) (e : expr)  : expr :=
   match e with
@@ -204,6 +299,10 @@ Fixpoint subst (x : string) (v : val) (e : expr)  : expr :=
 
 Definition subst' (mx : binder) (v : val) : expr → expr :=
   match mx with BNamed x => subst x v | BAnon => id end.
+
+(*|
+Two Gallina definitions `bin_op_eval` and `un_op_eval` define the semantics of these various pure operations, when the types of their arguments make sense.
+|*)
 
 Definition bin_op_eval (op: bin_op) (v1 v2: val) : option val :=
   match op with
@@ -229,6 +328,11 @@ Global Arguments state_upd_heap _ !_ /.
 
 Inductive observation :=.
 
+(*|
+The main semantics of simp_lang. `head_step e σ κs e' σ' efs` says that expression `e` in state `σ` reduces to `e'` and state `σ'` while forking threads `efs`. The observations `κs` are related to prophecy variables and are unused here (in fact observation is an empty inductive to `κs = []`).
+|*)
+
+
 Inductive head_step : expr → state → list observation → expr → state → list expr → Prop :=
   | RecS f x e σ :
     head_step (Rec f x e) σ [] (Val $ RecV f x e) σ []
@@ -249,13 +353,13 @@ Inductive head_step : expr → state → list observation → expr → state →
   | ForkS e σ:
     head_step (Fork e) σ [] (Val $ LitV LitUnit) σ [e]
   | AllocS v σ l :
-    (σ.(heap) !! l = None) →
+    σ.(heap) !! l = None →
     head_step (HeapOp AllocOp (Val v) (Val $ LitV LitUnit)) σ
               []
               (Val $ LitV $ LitInt l) (state_upd_heap <[l := v]> σ)
               []
   | LoadS v σ l :
-    (σ.(heap) !! l = Some v) →
+    σ.(heap) !! l = Some v →
     head_step (HeapOp LoadOp (Val $ LitV $ LitInt l) (Val $ LitV LitUnit)) σ
               []
               (Val $ v) σ
@@ -269,26 +373,29 @@ Inductive head_step : expr → state → list observation → expr → state →
               []
   .
 
+(*|
+We have to prove a few properties that show `fill_item` and `head_step` is reasonable for `ectxi_language`.
+|*)
+
+
 Global Instance fill_item_inj Ki : Inj (=) (=) (fill_item Ki).
-Proof. induction Ki; intros ???; simplify_eq/=; auto with f_equal. Qed.
+Proof. destruct Ki; intros ???; simplify_eq/=; auto with f_equal. Qed.
 
 Lemma fill_item_val Ki e :
   is_Some (to_val (fill_item Ki e)) → is_Some (to_val e).
-Proof. intros [v ?]. induction Ki; simplify_option_eq; eauto. Qed.
+Proof. intros [v ?]. destruct Ki; simplify_option_eq; eauto. Qed.
 
 Lemma val_head_stuck e1 σ1 κ e2 σ2 efs : head_step e1 σ1 κ e2 σ2 efs → to_val e1 = None.
 Proof. destruct 1; naive_solver. Qed.
 
 Lemma head_ctx_step_val Ki e σ1 κ e2 σ2 efs :
   head_step (fill_item Ki e) σ1 κ e2 σ2 efs → is_Some (to_val e).
-Proof. revert κ e2. induction Ki; inversion_clear 1; simplify_option_eq; eauto. Qed.
+Proof. destruct Ki; inversion_clear 1; simplify_option_eq; eauto. Qed.
 
 Lemma fill_item_no_val_inj Ki1 Ki2 e1 e2 :
   to_val e1 = None → to_val e2 = None →
   fill_item Ki1 e1 = fill_item Ki2 e2 → Ki1 = Ki2.
-Proof.
-  revert Ki1. induction Ki2; intros Ki1; induction Ki1; naive_solver eauto with f_equal.
-Qed.
+Proof. destruct Ki1, Ki2; naive_solver eauto with f_equal. Qed.
 
 Definition fresh_locs (ls : gset loc) : loc :=
   set_fold (λ k r, (1 + k) `max` r) 1 ls.
@@ -302,6 +409,8 @@ Proof.
     set_solver by eauto with lia.
 Qed.
 
+(** this theorem will be needed to show that allocation is never stuck when we
+prove a WP for it *)
 Lemma alloc_fresh v σ :
   let l := fresh_locs (dom (gset _) σ.(heap)) in
   head_step (HeapOp AllocOp (Val v) (Val $ LitV $ LitUnit)) σ []
@@ -313,6 +422,13 @@ Proof.
   by apply fresh_locs_fresh.
 Qed.
 
+(*|
+This is really where we instantiate the language, by constructing a "mixin" and then using some canonical structures to build the full record.
+
+You can see that the mixin uses `fill_item` and `head_step` as the core of the semantics. It uses `of_val` and `to_val` to define a number of related notions like reducible and not-stuck and such.
+|*)
+
+
 Lemma simp_lang_mixin : EctxiLanguageMixin of_val to_val fill_item head_step.
 Proof.
   split; apply _ || eauto using to_of_val, of_to_val, val_head_stuck,
@@ -323,15 +439,3 @@ Qed.
 Canonical Structure simp_ectxi_lang := EctxiLanguage simp_lang_mixin.
 Canonical Structure simp_ectx_lang := EctxLanguageOfEctxi simp_ectxi_lang.
 Canonical Structure simp_lang := LanguageOfEctx simp_ectx_lang.
-
-Lemma to_val_fill_some K e v : to_val (fill K e) = Some v → K = [] ∧ e = Val v.
-Proof.
-  intro H. destruct K as [|Ki K]; first by apply of_to_val in H. exfalso.
-  assert (to_val e ≠ None) as He.
-  { intro A. by rewrite fill_not_val in H. }
-  assert (∃ w, e = Val w) as [w ->].
-  { destruct e; try done; eauto. }
-  assert (to_val (fill (Ki :: K) (Val w)) = None).
-  { destruct Ki; simpl; apply fill_not_val; done. }
-  by simplify_eq.
-Qed.
